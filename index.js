bb235bc90863c2f471f2671afe27f68002eaa4a4
austinckohler <austinckohler@gmail.com>
2020-07-17 19:25:39 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+let scores, roundScore, activePlayer;
+
+scores = [0, 0];
+roundScore = 0;
+activePlayer = 0;
+
+// document.querySelector("#current-" + activePlayer).textContent = dice;
+
+let score = document.querySelector("#score-0").textContent;
+
+roll = document.querySelector(".btn-roll");
+roll.addEventListener("click", function () {
+  let dice = Math.floor(Math.random() * 6 + 1);
+  let diceImage = document.querySelector(".dice");
+  diceImage.style.display = "block";
+  diceImage.src = "dice-" + dice + ".png";
+});
+
+document.querySelector("#score-0").textContent = "0";
+document.querySelector("#score-1").textContent = "0";
