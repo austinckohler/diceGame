f8855575f0f2d49791396a4236b094866f8459d5
austinckohler <austinckohler@gmail.com>
2020-07-22 12:49:05 -0600

    adding event listeners and u[dating dice on roll dice

--- index.js
+++ index.js
@@ -3,20 +3,24 @@ let scores, roundScore, activePlayer;
 scores = [0, 0];
 roundScore = 0;
 activePlayer = 0;
-
-// document.querySelector("#current-" + activePlayer).textContent = dice;
-
-let score = document.querySelector("#score-0").textContent;
+let firstPlayerScore = document.querySelector("#score-0");
+let firstPlayerCurrentScore = document.querySelector("#current-0");
+let secondPlayerScore = document.querySelector("#score-1");
+let secondPlayerCurrentScore = document.querySelector("#current-1");
 
 roll = document.querySelector(".btn-roll");
-roll.addEventListener("click", function () {
+roll.addEventListener("click", () => {
+  //creates a random number 1-6
   let dice = Math.floor(Math.random() * 6 + 1);
+  //displays the correct image result
   let diceImage = document.querySelector(".dice");
   diceImage.style.display = "block";
-  diceImage.src = "dice-" + dice + ".png";
+  diceImage.src = "/photos/dice-" + dice + ".png";
+  //update round score if dice rolled !== 1
   dice;
 });
 
-score.textContent = "0";
-document.querySelector("#score-1").textContent = "0";
-document.append("scoore");
+firstPlayerScore.textContent = "0";
+firstPlayerCurrentScore.textContent = "0";
+secondPlayerScore.textContent = "0";
+secondPlayerCurrentScore.textContent = "0";
